0378ff735ceb72a1bc5a15ae8f333652b9ee409d
Kamalov Artur <artur.kamalov@bravedevelopers.com>
2021-04-12 09:52:36 +0400

    fix: FIX width with albumms container in IE11

--- src/сomponents/author/ui/author.information.albums.jsx
+++ src/сomponents/author/ui/author.information.albums.jsx
@@ -6,7 +6,7 @@ import { AuthorInformaitonAlbumsList } from "./author.informaiton.albums.list"
 import infoIcon from "images/information.png"
 
 export const AuthorInformationAlbums = ({ albums, openModal }) => (
-  <Box mt={5}>
+  <Box mt={5} flex = "1 1 100%">
     <Flex>
       <Heading fontSize={3}>Albums</Heading>
       <Image src={infoIcon} height={25} width={25} ml={2} color="grey" />
